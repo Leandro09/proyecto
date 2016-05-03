3b6a634d127d1b3a01c12d82d2baf8c3a808d4b6
aivaco <aivaco@outlook.com>
2016-05-02 23:05:25 -0600

    Cambios
    Cambios para que funcionen botones y otros.

--- Proyecto/Proyecto/Form1.cs
+++ Proyecto/Proyecto/Form1.cs
@@ -16,5 +16,11 @@ namespace Proyecto
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tarea prueba = new tarea();
+            prueba.leerInstruccion();
+        }
     }
 }
